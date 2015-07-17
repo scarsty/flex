45173a952001146a0718c38fcadeee2c74319f93
scarsty <scarsty@gmail.com>
2015-07-18 00:46:35 +0800

    gai wen jian ming

---
+++ flex_m/parameters.f90
@@ -0,0 +1,49 @@
+module parameters
+
+    implicit none
+    ! MPI variables
+    INTEGER rank, size
+    ! Timing variables
+    Real start_time, end_time
+    Real last_it_time, this_it_time
+
+    ! 化学势, 占据数
+    real mu, n_density
+
+
+    ! 目标占据数, 收敛误差
+    REAL target_density, density_tol
+
+    ! U, U', J, J' for H1
+    REAL h1_U, h1_Up, h1_J, h1_Jp
+
+    ! 温度
+    real T, T_beta
+
+    ! 保存的自能函数
+    LOGICAL sigma_input, sigma_output
+    CHARACTER*128 sigma_input_file, sigma_output_file
+
+    ! 自能函数收敛判据
+    REAL sigma_tol
+    INTEGER max_it
+    REAL alpha
+    INTEGER alpha_scheme
+
+    ! 自旋态
+    integer spin_state
+
+
+
+#ifdef USE_MPI
+    INTEGER ierr
+#endif /* USE_MPI */
+
+
+    namelist /basic/ T, target_density, density_tol, mu,&
+        h1_U, h1_Up, h1_J, h1_Jp,&
+        sigma_input,  sigma_input_file,&
+        sigma_output, sigma_output_file,&
+        sigma_tol, max_it, alpha,  alpha_scheme, spin_state
+
+end module parameters
---
+++ flex_m/parameters2.f90
@@ -0,0 +1,43 @@
+!数组专用
+module parameters2
+
+    use Constants
+    implicit none
+
+    public
+    ! 格林函数, 反常格林函数
+    ! 自能函数, 反常自能函数
+    complex, dimension (nb, nb, nk, -nomega:nomega) :: G, F, sigma, delta, G0, sigma0, G1
+    complex, dimension (nb, nb, nk, -ntau:ntau) :: G_tau, sigma_tau, delta_tau
+
+    ! 极化率, susceptibilities, effective interactions
+    complex, dimension (nb*nb, nb*nb, nk, -nomega:nomega) :: chi_0, chi_s, chi_c, V, V_s
+    complex, dimension (nb*nb, nb*nb, nk, -ntau:ntau) :: chi_0_tau, V_tau, V_s_tau
+
+    ! 交换能, 单位矩阵
+    complex, dimension (nb*nb, nb*nb):: U_s, U_c, U_ud, U_uu, I_chi
+
+    ! H0
+    complex, dimension (nb, nb, -2:2, -2:2) :: h0_r
+    complex, dimension (nb, nb, nk):: h0_k
+
+    ! k空间对应
+    real, dimension (nk, 2) :: k
+    ! 两个k的差对应的k
+    integer, dimension (nk, nk) :: k_minus, k_plus
+
+    ! 计算g0的辅助
+    complex, dimension (nb, nb) :: I_g0
+    real, dimension (nb, nb) :: u_g0
+
+        ! 计算chi的辅助
+    complex, dimension (nb*nb, nb*nb) :: chi_0_, chi_c_, chi_s_, Iminuschi_0_
+
+    ! 傅里叶变换, 反傅里叶变换的辅助, f/b means fermi and bose freq
+    complex, dimension (-nomega:nomega, -ntau:ntau) :: dft_f, dft_b
+    complex, dimension (-ntau:ntau, -nomega:nomega) :: idft_f, idft_b
+
+    complex, dimension (nk, -nomega:nomega) :: dft_omega
+    complex, dimension (nk, -ntau:ntau) :: dft_tau
+
+end module parameters2
