31a8f330189a156f7d1fa35e5873f1638b57cd79
scc__ <scarscc@gmail.com>
2015-10-22 17:52:53 +0800

    傅里叶变换重新设计

--- flex_m/myfunctions.f90
+++ flex_m/myfunctions.f90
@@ -241,11 +241,9 @@ contains
 
         do l=1,N; do m=1,N
             dft_in = input(l,m,:,:,:)
-
             plan=fftw_plan_dft_3d(totalnomega, nkx, nky , dft_in, dft_out, direction2, FFTW_ESTIMATE)
             call fftw_execute_dft(plan, dft_in, dft_out)
             call fftw_destroy_plan(plan)
-
             output(l,m,:,:,:) = dft_out
         enddo; enddo
         ! 卷积结果归一化
